1a0e7292f10235e2ddb8d90d7bd2fc5820d094ac
Jacob Dent <murkymicrobe88@gmail.com>
2025-05-19 17:35:11 -0400

    First verison of the birdcounting script using the yolo model

--- src/birdcounting/birdcounting.py
+++ src/birdcounting/birdcounting.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import os
 import csv
+from ultralytics import YOLO
 
 
 def main():
+
+    # Load the Yolo model
+    model = YOLO("/home/jacob/BirdBotV1/localmodeltest/100epochSet2.pt")
     
     # Folder Labels
     image_Folder = "unclassifiedImages/"
@@ -28,59 +32,39 @@ def main():
             writer.writerow(labels)
 
     # Read csv as dataframe
-    birdbata = pd.read_csv(results_Path)
+    birddata = pd.read_csv(results_Path)
     
     
-    
-    
-    
-    pass
+    # Loop through all the images in the unclassified folder
+    for filename in os.listdir(image_Folder):
+
+        # Get date and time information of the image
+        year = filename[:4]
+        month = filename[4:6]
+        day = filename[6:8]
+        hour = filename[8:10]
+
+        # Get a path to the image
+        file_path = os.path.join(image_Folder, filename)
+
+        # Run the model over the image
+        results = model(file_path)
+
+        # Parse results
+        boxes = results[0].boxes
+
+        # Cycle through spotted birds
+        for box in boxes:
+            class_id = int(box.cls.item())
+            bird_species = results[0].names[class_id]
+
+            # Add entry
+            birddata.loc[-1] = [bird_species, year, month, day, hour]
+            birddata.index = birddata.index + 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Save updated data frame
+    birddata.to_csv(results_Path)
 
 
 
